4427f0935388f4b78bad06762d48599f5b41e87f
kit486759 <kit486759@gmail.com>
2021-05-03 23:19:52 -0700

    catalog

--- index.js
+++ index.js
@@ -7,203 +7,144 @@ let nowPlayingList = nowPlaying.getElementsByTagName("li")
 let upcoming = document.getElementsByClassName("row upcoming")[0]
 let upcomingList = upcoming.getElementsByTagName("li")
 
-// let displayList = () => {
-//     console.log(`displaylist fire`)
-//     return new Promise((resolve, reject) => {
+let actionGenres = document.getElementsByClassName("dropdown-item action")[0]
 
+let dropdownMenu = document.getElementsByClassName("dropdown-menu")[0]
+let dropdownItem = document.getElementsByClassName("dropdown-item")
 
-//         for (let i = 0; i < nowPlayingList.length; i++) {
-
-//             nowPlayingList[i].style.display = ""
-
-//         }
-
-
-//         for (let i = 0; i < openingThisWeekList.length; i++) {
-
-//             openingThisWeekList[i].style.display = ""
-//         }
-
-
-//         let error = false
-
-//         if (!error) {
-//             resolve()
-//         } else {
-//             reject(alert("Something went wrong. Please refresh the page."))
-//         }
-//     })
-// }
-
-// let search = () => {
-//     console.log(`search fire`)
-
-
-//     return new Promise((resolve, reject) => {
-
-//         for (let i = 0; i < nowPlayingList.length; i++) {
-//             // console.log(nowPlayingList[i].innerText)
-//             let search = searchInput.value.toUpperCase()
-//             let data = nowPlayingList[i].innerText || nowPlayingList[i].textContent
-
-//             // console.log(search)
-//             // console.log(data.toUpperCase())
-
-//             if (data.toUpperCase().indexOf(search) > -1) {
-//                 nowPlayingList[i].style.display = ""
-//                 // nowPlayingList[i].classList.add("searchResult")
-//             } else {
-//                 nowPlayingList[i].style.display = "none"
-//             }
-
-//         }
-
-//         for (let i = 0; i < openingThisWeekList.length; i++) {
-//             // console.log(nowPlayingList[i].innerText)
-//             let search = searchInput.value.toUpperCase()
-//             let data = openingThisWeekList[i].innerText || openingThisWeekList[i].textContent
-
-//             // console.log(search)
-//             // console.log(data.toUpperCase())
-
-//             if (data.toUpperCase().indexOf(search) > -1) {
-//                 openingThisWeekList[i].style.display = ""
-
-//             } else {
-//                 openingThisWeekList[i].style.display = "none"
-//             }
-
-//         }
-
-//         let error = false
-
-//         if (!error) {
-//             resolve()
-//         } else {
-//             reject(alert("Something went wrong. Please refresh the page."))
-//         }
-
-//     })
-
-
-// }
-
-
-
-// let noResult = () => {
-
-//     console.log(`no result fire`)
-
-//     for (let i = 0; i < nowPlayingList.length; i++) {
-
-//         if (nowPlayingList[0].style.display == "none") {
-
-//             let ul = nowPlaying.getElementsByTagName("ul")[0]
-//             console.log(ul)
-//             let li = document.createElement("li")
-//             li.innerHTML = (`<h3>No result</h3>`)
-//             li.style = ""
-//             li.className = "noResult"
-//             ul.prepend(li)
-
-//         }
-//     }
-
-//     for (let i = 0; i < openingThisWeekList.length; i++) {
-
-//         if (openingThisWeekList[0].style.display == "none") {
-
-//             let ul = openingThisWeek.getElementsByTagName("ul")[0]
-//             console.log(ul)
-//             let li = document.createElement("li")
-//             li.innerHTML = (`<h3>No result</h3>`)
-//             li.style = ""
-//             li.className = "noResult"
-//             ul.prepend(li)
-
-//         }
-//     }
-
-// }
-
-// async function clickSearch() {
-//     await displayList();
-//     await search();
-
-//     if (nowPlayingList[0].classList == "noResult") {
-//         nowPlayingList[0].remove()
-//         noResult();
-//     }
-
-//     // if(openingThisWeekList[0].classList == "noResult"){
-//     //     openingThisWeekList[0].remove()
-//     //     noResult();
-//     // }
-
-// }
-
-
-// // if (nowPlayingList.classList.contains("searchResult") == false){
-// //     alert ("no result")
-// // }
-
-// let test = () => {
-
-//     let ul = nowPlaying.getElementsByTagName("ul")[0]
-//     console.log(ul)
-//     let li = document.createElement("li")
-//     ul.appendChild(li)
-// }
-
-// searchBtn.addEventListener("click", clickSearch)
-
-// fetch("https://api.themoviedb.org/3/movie/latest?api_key=8ff5e2fbb1b643a55f0256bb89a8a192")
 let api = {
     url: "https://api.themoviedb.org/3/movie/",
     key: "api_key=8ff5e2fbb1b643a55f0256bb89a8a192",
 
 }
 
-
-
 let apiImg = {
     url: "https://image.tmdb.org/t/p/w200"
 }
 
 
-fetch(`${api.url}now_playing?${api.key}&language=en-US&page=1`)
-    .then(response => {
-        if (response.status !== 200) {
-            return console.log(`Look like there was a problem, status code ${response.status}`)
-        }
-        return response.json()
-    })
-    .then(data => {
-        return nowPlayingDisplay(data)
-    })
-    .catch(err => {
-        console.log(`Fetch error: ${err}`)
-    })
+// fetch(`${api.url}now_playing?${api.key}&language=en-US&page=1`)
+//     .then(response => {
+//         if (response.status !== 200) {
+//             return console.log(`Look like there was a problem, status code ${response.status}`)
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//         return nowPlayingDisplay(data)
+//     })
+//     .catch(err => {
+//         console.log(`Fetch error: ${err}`)
+//     })
 
-fetch(`${api.url}upcoming?${api.key}&language=en-US&page=1&region=CA`)
-    .then(response => {
-        if (response.status !== 200) {
-            return console.log(`Look like there was a problem, status code ${response.status}`)
+// fetch(`${api.url}upcoming?${api.key}&language=en-US&page=1&region=CA`)
+//     .then(response => {
+//         if (response.status !== 200) {
+//             return console.log(`Look like there was a problem, status code ${response.status}`)
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//         return upcomingDisplay(data)
+//         // console.log(`up coming`)
+//         // console.log(data)
+//     })
+//     .catch(err => {
+//         console.log(`Fetch error: ${err}`)
+//     })
+
+
+
+let urls = [
+    (`${api.url}now_playing?${api.key}&language=en-US&page=1`),
+    (`${api.url}upcoming?${api.key}&language=en-US&page=1&region=CA`),
+    (`https://api.themoviedb.org/3/genre/movie/list?${api.key}&language=en-US`)
+]
+
+let initDisplay = () => {
+    Promise.all(urls.map(url =>
+        fetch(url).then(resp => resp.json())
+    ))
+        .then(data => {
+            nowPlayingDisplay(data[0])
+            upcomingDisplay(data[1])
+            genreListDisplay(data[2])
+        })
+
+}
+
+let genreListDisplay = (genre) => {
+
+    for (let i = 0; i < genre.genres.length; i++) {
+
+        console.log(genre.genres[i])
+
+        let ul = dropdownMenu
+        let li = document.createElement("li")
+        let a = document.createElement("a")
+        a.className = (`dropdown-item ${genre.genres[i].id} ${genre.genres[i].name}`)
+        ul.appendChild(li)
+        li.appendChild(a)
+        a.innerText = (`${genre.genres[i].name}`)
+        let id = document.createAttribute("id")
+        id.value = (`${genre.genres[i].id}`)
+        a.setAttributeNode(id)
+        dropdownItem[i].addEventListener("click", clickGenre)
+    }
+}
+
+clickGenre = (e) => {
+    //    if(nowPlayingList[0].className == "nowPlayingMovie noResult"){
+    //     nowPlayingList[0].remove()
+    //    }
+    initDisplay()
+    console.log(e.target.id)
+
+    let targetId = e.target.id
+    let title = nowPlaying.getElementsByTagName("h4")[0]
+    // console.log(title.textContent)
+    // console.log(title.innerText)
+    title.innerText = (e.target.innerText)
+    // let selectGenre = e.target.id
+    console.log(nowPlayingList[0].id)
+    console.log(upcomingList[0].id)
+
+    for (i = 0; i < nowPlayingList.length; i++) {
+
+        if (nowPlayingList[i].id.includes(targetId)) {
+            nowPlayingList[i].style.display = ""
+
+        } else if (!nowPlayingList[i].id.includes(targetId)) {
+            nowPlayingList[i].style.display = "none"
         }
-        return response.json()
-    })
-    .then(data => {
-        return upcomingDisplay(data)
-        // console.log(`up coming`)
-        // console.log(data)
-    })
-    .catch(err => {
-        console.log(`Fetch error: ${err}`)
-    })
+    }
+
+    for (i = 0; i < upcomingList.length; i++) {
+
+        if (upcomingList[i].id.includes(targetId)) {
+            upcomingList[i].style.display = ""
+
+        } else if (!upcomingList[i].id.includes(targetId)) {
+            upcomingList[i].style.display = "none"
+        }
+    }
+
+    // if (nowPlayingList[0].style.display == "none") {
+    //     let li = document.createElement("li")
+    //     let ul = nowPlaying.getElementsByTagName("ul")[0]
+    //     li.className = "nowPlayingMovie noResult"
+    //     li.innerText = ("No result")
+    //     ul.prepend(li)
+    // }
+
+
+}
+
+
 
 let nowPlayingDisplay = (movie) => {
     console.log(movie)
-    console.log(`${apiImg.url}${movie.results[0].poster_path}`)
+    // console.log(`${apiImg.url}${movie.results[0].poster_path}`)
 
     for (let i = 0; i < movie.results.length; i++) {
 
@@ -211,18 +152,21 @@ let nowPlayingDisplay = (movie) => {
         let li = document.createElement("li")
         li.className = "nowPlayingMovie"
         ul.appendChild(li)
+        let id = document.createAttribute("id")
+        id.value = (`${movie.results[i].genre_ids}`)
+        li.setAttributeNode(id)
         let img = document.createElement("img")
         img.setAttribute("src", (`${apiImg.url}${movie.results[i].poster_path}`))
         li.appendChild(img)
         let p = document.createElement("p")
-        p.innerHTML = (`${movie.results[i].title}`)
+        p.innerHTML = (`${movie.results[i].title}${movie.results[i].genre_ids}`)
         li.appendChild(p)
     }
 }
 
 let upcomingDisplay = (movie) => {
     console.log(movie)
-    console.log(`${apiImg.url}${movie.results[0].poster_path}`)
+    // console.log(`${apiImg.url}${movie.results[0].poster_path}`)
 
     for (let i = 0; i < movie.results.length; i++) {
 
@@ -230,6 +174,9 @@ let upcomingDisplay = (movie) => {
         let li = document.createElement("li")
         li.className = "upcomingMovie"
         ul.appendChild(li)
+        let id = document.createAttribute("id")
+        id.value = (`${movie.results[i].genre_ids}`)
+        li.setAttributeNode(id)
         let img = document.createElement("img")
         img.setAttribute("src", (`${apiImg.url}${movie.results[i].poster_path}`))
         li.appendChild(img)
@@ -238,6 +185,16 @@ let upcomingDisplay = (movie) => {
         li.appendChild(p)
     }
 }
+
+
+
+
+initDisplay()
+
+
+
+
+
 
  // title: movie.results[0].poster_path
     // title: movie.results[0].title
